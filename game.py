a8e49b83304b85ec9ac525d06aeb1a4b9d1c1018
abul <abulsiddiqui89@gmail.com>
2023-07-06 14:10:05 -0400

    sell tools added

--- game.py
+++ game.py
@@ -1,87 +1,88 @@
+
 money = 0
-tool = "teeth"
+tools = []
+tool_prices = {
+    "teeth": 0,
+    "scissors": 5,
+    "push lawnmower": 25,
+    "battery-powered lawnmower": 250,
+    "team of starving students": 500
+}
+tool_earnings = {
+    "teeth": 1,
+    "scissors": 5,
+    "push lawnmower": 50,
+    "battery-powered lawnmower": 100,
+    "team of starving students": 250
+}
 
 
 def display_status():
- print(f"\nMoney: ${money}")
- print(f"Tool: {tool}")
- print()
+    print(f"\nMoney: ${money}")
+    print(f"Tools: {tools}")
+
 
 def reset_game():
-    global money, tool
+    global money, tools
     money = 0
-    tool = "teeth"
-    print("Game has been reset. You're back to using your teeth.")
+    tools = []
+    print("Game has been reset.")
+
+
+# def sell_tool(tool_name):
+#     global money, tools
+#     if tool_name in tools:
+#         tool_value = tool_prices[tool_name] // 2
+#         money += tool_value
+#         tools.remove(tool_name)
+#         print(f"You sold {tool_name} for ${tool_value}.")
+#     else:
+#         print("You don't have that tool to sell.")
 
 
 display_status()
 
 while money < 1000:
-    
-    action = input("\nWhat do you want to do? (cut/buy/reset/quit): ")
+    action = input("\nWhat do you want to do? (cut/buy/sell/reset/quit): ")
     display_status()
     if action == "cut":
-        if tool == "teeth":
-            money += 1
-            print("You cut the grass using your teeth and earned $1.")
-        elif tool == "scissors":
-            money += 5
-            print("You cut the grass using the rusty scissors and earned $5.")
-        elif tool == "push lawnmower":
-            money += 50
-            print("You cut the grass using the old-timey push lawnmower and earned $50.")
-        elif tool == "battery-powered lawnmower":
-            money += 100
-            print("You cut the grass using the fancy battery-powered lawnmower and earned $100.")
-        elif tool == "team of starving students":
-            money += 250
-            print("You cut the grass using the team of starving students and earned $250.")
-        else:
-            print("Invalid action. Please try again.")
-        display_status()      
+        earning = sum([tool_earnings[tool] for tool in tools])
+        money += earning
+        print(f"You cut the grass using your tools and earned ${earning}.")
     elif action == "buy":
-        if tool == "teeth":
-            if money >= 5:
-                money -= 5
-                tool = "scissors"
-                print("You bought a pair of rusty scissors!")
+        tool_name = input("Which tool do you want to buy? (scissors/push lawnmower/battery-powered lawnmower/team of starving students): ")
+        if tool_name in tool_prices:
+            if money >= tool_prices[tool_name]:
+                money -= tool_prices[tool_name]
+                tools.append(tool_name)
+                print(f"You bought a {tool_name}!")
             else:
-                print("You don't have enough money to buy scissors.")
-        elif tool == "scissors":
-            if money >= 25:
-                money -= 25
-                tool = "push lawnmower"
-                print("You bought an old-timey push lawnmower!")
-            else:
-                print("You don't have enough money to buy a push lawnmower.")
-        elif tool == "push lawnmower":
-            if money >= 250:
-                money -= 250
-                tool = "battery-powered lawnmower"
-                print("You bought a fancy battery-powered lawnmower!")
-            else:
-                print("You don't have enough money to buy a battery-powered lawnmower.")
-        elif tool == "battery-powered lawnmower":
-            if money >= 500:
-                money -= 500
-                tool = "team of starving students"
-                print("You hired a team of starving students!")
-            else:
-                print("You don't have enough money to hire a team of starving students.")
+                print("You don't have enough money to buy that tool.")
         else:
-            print("Invalid action. Please try again.")
-    elif action == "reset":
+            print("Invalid tool. Please try again.")
+
+    elif action == "sell":
+        tool_name = input("Which tool do you want to sell? (scissors/push lawnmower/battery-powered lawnmower/team of starving students): ")
+    if tool_name in tools:
+        tool_value = tool_prices[tool_name] // 2
+        money += tool_value
+        tools.remove(tool_name)
+        print(f"You sold {tool_name} for ${tool_value}.")   
+    else:
+        print("You don't have that tool to sell.")
+    if action == "reset":
         reset_game()
     elif action == "quit":
         break
     else:
         print("Invalid action. Please try again.")
+    
+    display_status()
 
 
-
-if money >= 1000 and tool == "team of starving students":
+if money >= 1000:
     print("\nCongratulations! You won the game!")
 else:
     print("\nGame over!")
 
-
+display_status()
